d36ad2991a0680899a42778ef86bd0cc0a6f823d
Jennifer Bono <jennb24@gmail.com>
2018-02-24 17:33:50 -0600

    fix?

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./components/pages/Home";
 import Navbar from "./components/Navbar";
 
 
