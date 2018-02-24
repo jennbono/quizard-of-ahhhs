9ffcf364148e03d9f01031673a06346414e9b7c6
nbnsireesha <nbnsireesha@gmail.com>
2018-02-24 17:45:06 -0600

    working navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+//import Home from "./pages/Home";
 import Home from "./pages/Home";
+//import Navbar from "./components/Navbar";
 import Navbar from "./components/Navbar";
 
 
