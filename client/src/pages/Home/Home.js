5a7419d99839d785722dfe4559c19d06ca6b6814
Jennifer Bono <jennb24@gmail.com>
2018-02-26 20:11:30 -0600

    changes

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -13,7 +13,7 @@ class Home extends Component {
         <Container fluid>
           <Row>
             <Col size="md-6">
-              <img className="text-center" src="img/quizard_of_ahhs.png" alt="Quizard of Ahhhs... Logo" height="250" />
+              <img src="img/quizard_of_ahhs.png" alt="Quizard of Ahhhs... Logo" height="250" />
               <Card>
                 <CardHeader><h1>Rules of the Game</h1></CardHeader>
                 <CardBody>
