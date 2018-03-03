f93a0a8903d182242bec84ed9c1d7d0e354cfcc3
slizbethellis <slizbethellis@gmail.com>
2018-03-02 22:08:10 -0600

    basic timer working, also fixed quotation mark display issue

--- client/src/pages/Question/Question.js
+++ client/src/pages/Question/Question.js
@@ -3,6 +3,10 @@ import Navbar from "../../components/Nav";
 import { Col, Container, Row } from "../../components/Grid";
 import { Card, CardBody, CardHeader } from "../../components/Card";
 import API from "../../utils/api";
+// fixes the appearance of quotation marks in questions
+const Entities = require('html-entities').XmlEntities;
+ 
+const entities = new Entities();
 
 class Question extends Component {
   constructor(props) {
@@ -13,7 +17,7 @@ class Question extends Component {
       answers: [],
       answerClicked: null,
       correctAnswerIndex: null,
-      questionNum: 1,
+      questionNum: 0,
       totalAnswered: 0,
       totalCorrect: 0,
       totalWrong: 0,
@@ -25,17 +29,30 @@ class Question extends Component {
     this.showQuestion();
   }
 
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.showQuestion(),
+      1000 * 25
+    );
+  }
+
   componentWillUnmount() {
 
   }
 
   showQuestion() {
-    API.getQuestions()
+    let questionNum = this.state.questionNum;
+    if (questionNum > 14) {
+      this.endRound();
+    }
+    else {
+      API.getQuestions()
       .then(res => {
-        this.setState({ question: res.data.results[0] });
+        this.setState({ question: res.data.results[0], questionNum: questionNum+1, questionOn: true });
         this.makeAnswerArray();
       })
       .catch(err => console.log(err));
+    }
   }
 
   makeAnswerArray() {
@@ -75,6 +92,10 @@ class Question extends Component {
       }
   }
 
+  endRound() {
+    clearInterval(this.timerID);
+  }
+
   render() {
     return (
       <div className="background">
@@ -89,14 +110,14 @@ class Question extends Component {
                 <Card>
                   <CardHeader><h1 className="text-center">Question {this.state.questionNum}</h1></CardHeader>
                   <CardBody>
-                    <h4 className="text-center">{this.state.question.question}</h4>
+                    <h4 className="text-center">{entities.decode(this.state.question.question)}</h4>
                   </CardBody>
                 </Card>
                 <div className="text-center">
                   {this.state.answers.map((option, index) => {
                     return (
                       <div key={index}>
-                        <button type="button" className="btn btn-outline-dark answer-btn text-center" onClick={() => this.scoreAnswer(index)}>{option}</button>
+                        <button type="button" className="btn btn-outline-dark answer-btn text-center" onClick={() => this.scoreAnswer(index)}>{entities.decode(option)}</button>
                         <br />
                       </div>
                     );
@@ -112,8 +133,8 @@ class Question extends Component {
                 <Card>
                   <CardHeader><h1 className="text-center">Question {this.state.questionNum}</h1></CardHeader>
                   <CardBody>
-                    <h4 className="text-center">{this.state.question.question}</h4>
-                    <h6 className="text-center">The correct answer is: {this.state.answers[this.state.correctAnswerIndex]}</h6>
+                    <h4 className="text-center">{entities.decode(this.state.question.question)}</h4>
+                    <h6 className="text-center">The correct answer is: {entities.decode(this.state.answers[this.state.correctAnswerIndex])}</h6>
                     <p className="text-center"><strong>Total Answered:</strong> {this.state.totalAnswered} out of {this.state.questionNum}</p>
                     <p className="text-center"><strong>Total Correct:</strong> {this.state.totalCorrect}</p>
                     <p className="text-center"><strong>Total Wrong:</strong> {this.state.totalWrong}</p>
