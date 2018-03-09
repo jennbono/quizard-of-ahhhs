8057a5fb8b60690760687ef57b805c2b0b2fd2da
slizbethellis <slizbethellis@gmail.com>
2018-03-09 14:52:41 -0600

    alternate heroku buildpack

--- react-ui/src/components/Card/CardHeader.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export const CardHeader = props =>
-  <div className={window.location.pathname === "/winner" ? "card-header winner" : "card-header default"}>
-    {props.children}
-  </div>;
--- react-ui/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default {
-  getQuestions: function() {
-    const queryURL = "https://opentdb.com/api.php?amount=1&category=9&difficulty=hard&type=multiple" ;
-    return axios.get(queryURL);
-  }
-}
