be3f9c72360b13eec983b843ee9d00fa9804d24a
slizbethellis <slizbethellis@gmail.com>
2018-03-09 21:28:45 -0600

    alternate heroku buildpack

--- server/db/index.js
+++ server/db/index.js
@@ -8,7 +8,7 @@ const MONGO_LOCAL_URL = 'mongodb://localhost/quizardofahhhs'
 
 if (process.env.MONGODB_URI) {
 	mongoose.connect(process.env.MONGODB_URI)
-	MONGO_URL = 'mongodb://login:$gTIil7N602fVAfn4$3q@ds251598.mlab.com:51598/quizardofahhhs' || process.env.MONGODB_URI
+	MONGO_URL = process.env.MONGODB_URI
 } else {
 	mongoose.connect(MONGO_LOCAL_URL) // local mongo url
 	MONGO_URL = MONGO_LOCAL_URL
