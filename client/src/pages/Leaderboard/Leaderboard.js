6a0c384f893972d39a04612478de185fa442399f
jilljurgens <jurgensjill@gmail.com>
2018-03-11 09:42:46 -0500

    Leaderboard is working

--- client/src/pages/Leaderboard/Leaderboard.js
+++ client/src/pages/Leaderboard/Leaderboard.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { Redirect } from 'react-router-dom';
 import axios from "axios";
-//import Navbar from "../../components/Nav";
-//import { Col, Container, Row } from "../../components/Grid";
-//import { Card, CardBody, CardHeader } from "../../components/Card";
+import Navbar from "../../components/Nav";
+import { Col, Container, Row } from "../../components/Grid";
+import { Card, CardBody, CardHeader } from "../../components/Card";
 //import Routes from "../../../routes/userRoutes";
 //import API from "../../../server/auth";
 
@@ -27,69 +27,7 @@ class Leaderboard extends Component {
   }
   render() { 
 
-  //   return (
-  //     this.props.data.map((highscore, index) => (
-  //     <span className="indent" key={index}>
-  //         {index}
-  //     </span>
-  //     )
-  // ));
-
-  // var score = [];
-  // for (var i = 0; i < 5; i++) {
-  //   this.state.data.push(<span  key={i}></span>);
-  // }
-  // return (
-  //    <div>
-  //     {score}
-  //     "Some text value"
-  //    </div>
-  // );
-
-
-
-
-  // Object.entries(this.props.data).map(([name, highscore]) => {
-  //   console.log(name)
-  //   console.log(highscore)
-  //console.log(this.state.data)
-
-     // this.state.data.map((item,i) => <li key={i}>Test</li>)
-
-     //  this.state.data.map(function(name, i){
-    //   console.log('test');
-     
-    //   return <li key={i}>test</li>
-    // })
-
-  //   return (
-  //     <div>
-  //      {this.state.data.map((item, index) => (
-  //         <span className='indent' key={index} />
-  //      ))}
-  //      "Some text value"
-  //     </div>
-  //  );
-  // var indents = [];
-  // for (var i = 0; i < this.props.level; i++) {
-  //   indents.push(<span className='indent' key={i}></span>);
-  // }
-  // return indents;
-
-      
-      // Object.entries(this.props.data).map(([name, highscore]) => {
-      //   console.log(name)
-      // console.log(highscore)
-     
-
-
-      
-   
-    //   <div>
-    //     <p>User: {name}</p>
-    //     <p>High Score: {highscore}</p>
-    //  </div>
-    //)
+ 
     
     let result = this.state.data.map(score => ({ value: score.name, text: score.highscore }));
     console.log(result)
@@ -97,7 +35,17 @@ class Leaderboard extends Component {
     var resultScores;
     return (
       <div>
-        {resultScores = result.map(top => <li>{top.value}</li> )}
+         <Navbar  _logout={this._logout} loggedIn={this.state.loggedIn}/>
+         <Container fluid>
+            <Card>
+              <CardHeader><h1 className="text-center">Leaderboard </h1></CardHeader>
+                  <CardBody> 
+                    {resultScores = result.map(topScores => 
+                      <h5>{topScores.value}  {topScores.text} </h5> 
+                    )}
+                </CardBody>
+              </Card>
+            </Container>
       </div>
       
     );
