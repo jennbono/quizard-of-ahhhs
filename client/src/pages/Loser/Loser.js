4ae610ee6d24291ff597cebc04368d0fbec07333
Jennifer Bono <jennb24@gmail.com>
2018-03-12 10:26:27 -0500

    add text to loser page

--- client/src/pages/Loser/Loser.js
+++ client/src/pages/Loser/Loser.js
@@ -20,9 +20,8 @@ class Loser extends Component {
             <Col size="md-6">
               <img className="mx-auto d-block" src="img/lose_logo.png" alt="Loser Logo" height="250" />
               <Card>
-                <CardHeader className="default"><h1 className="text-center">Rules of the Game</h1></CardHeader>
                 <CardBody>
-                  <h3 className="text-center">Every player has 20 seconds to answer each question.  The player with the most questions correct at the end of 15 questions will be the winner and will enter Emerald Quizity.</h3>
+                  <h3 className="text-center">The Wicked Witch of the West has summoned her monkeys to fly you away from the Emerald Quizity!  Try again next time! </h3>
                 </CardBody>
               </Card>
               {/* Need to insert button to enter the Quizard of Ahhhs (questions page) ONLY IF LOGGED IN */}
