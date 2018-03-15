60e5ae41489044a9bffcff702dd9c22f062bc5da
Jennifer Bono <jennb24@gmail.com>
2018-03-14 20:58:49 -0500

    change game rules

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -22,7 +22,7 @@ class Home extends Component {
               <Card>
                 <CardHeader className="default"><h1 className="text-center">Rules of the Game</h1></CardHeader>
                 <CardBody>
-                  <h3 className="text-center">Every player has 10 seconds to answer each question.  The player with the most questions correct at the end of 15 questions will be the winner and will enter Emerald Quizity.</h3>
+                  <h3 className="text-center">Every player has 10 seconds to answer each question.  The players with fewer than 4 incorrect answers at the end of 15 questions will be the winner and will enter Emerald Quizity.</h3>
                 </CardBody>
               </Card>
               {/* Need to insert button to enter the Quizard of Ahhhs (questions page) ONLY IF LOGGED IN */}
